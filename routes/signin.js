64fbaf5212e711ecb54c357299f11057ba6d565b
Nguyen Hong Minh Nhat <1512293@hcmut.edu.vn>
2018-07-23 09:32:00 +0700

    Cai ESLint

---
+++ routes/signin.js
@@ -0,0 +1,10 @@
+var express = require('express')
+var router = express.Router()
+
+/* GET sign in page. */
+router.get('/', function (req, res, next) {
+  res.render('signin', { title: 'Express' }) 
+       
+})
+
+module.exports = router
