fb8681a693a1198ee974ec23cab7b7559be7fe28
Nguyen Hong Minh Nhat <1512293@hcmut.edu.vn>
2018-07-27 17:30:27 +0700

    Them chuc nang Search phim

--- public/javascripts/film/showListFilm.js
+++ public/javascripts/film/showListFilm.js
@@ -1,7 +1,36 @@
 (() => {
   const app = angular.module('Cinema-Web')
+  var vietnameseToAlias = function (alias) {
+    if (alias == null) return null;
+    var str = alias;
+    str = str.toLowerCase();
+    str = str.replace(/à|á|ạ|ả|ã|â|ầ|ấ|ậ|ẩ|ẫ|ă|ằ|ắ|ặ|ẳ|ẵ/g, 'a');
+    str = str.replace(/è|é|ẹ|ẻ|ẽ|ê|ề|ế|ệ|ể|ễ/g, 'e');
+    str = str.replace(/ì|í|ị|ỉ|ĩ/g, 'i');
+    str = str.replace(/ò|ó|ọ|ỏ|õ|ô|ồ|ố|ộ|ổ|ỗ|ơ|ờ|ớ|ợ|ở|ỡ/g, 'o');
+    str = str.replace(/ù|ú|ụ|ủ|ũ|ư|ừ|ứ|ự|ử|ữ/g, 'u');
+    str = str.replace(/ỳ|ý|ỵ|ỷ|ỹ/g, 'y');
+    str = str.replace(/đ/g, 'd');
+    return str;
+};
+  app.filter('filmFilter', function () {
+    return function (items) {
+      console.log(items)
+      if (!items)
+        return []
+      let result = items.filter(function (element, index, array) {
+        return vietnameseToAlias(element.name).includes(vietnameseToAlias($('#input-search').val())) || 
+        vietnameseToAlias(element.genre).includes(vietnameseToAlias($('#input-search').val())) ||
+        vietnameseToAlias(element.content).includes(vietnameseToAlias($('#input-search').val())) ||
+        COMMON.timeStampToString(element.releaseDate).includes(vietnameseToAlias($('#input-search').val()))
+      })
+      console.log(result)
+      return result
+    }
+  })
   app.controller('showListController', ['$scope', 'apiService', function ($scope, apiService) {
     let userid = $('#user-id').text()
+    $scope.resultList = []
     $scope.clickSignOut = function () {
       document.getElementById('LogoutDialog').style.display = 'block'
     }
@@ -17,6 +46,10 @@
     $scope.cancelDialog = function () {
       document.getElementById('LogoutDialog').style.display = 'none'
     }
+    $scope.toggleInputSearch = () => {
+      $('#input-search').animate({ width: 'toggle' }, 300)
+    }
+
     if (userid) {
       apiService.getUser(userid)
         .then(function (response) {
