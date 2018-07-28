20800b7b43a8824d84649ce21e55114459d796fc
Nguyen Hong Minh Nhat <1512293@hcmut.edu.vn>
2018-07-28 13:16:12 +0700

    Chinh API dang nhap cho mobile (tra ve fail khi sai email hoac pass)

--- routes/api/routes/authentication.js
+++ routes/api/routes/authentication.js
@@ -9,9 +9,9 @@ router.post('/signup', authController.signUp)
 router.post('/signin', function (req, res, next) {
   passport.authenticate('local', async (err, user, info) => {
     console.log(err, user, info)
-    if (err) return res.send(err)
+    if (err) return res.status(500).send(err)
     if (!user) {
-      return res.send(responseStatus.Code401({ errorMessage: responseStatus.WRONG_EMAIL_OR_PASSWORD }))
+      return res.status(401).send(responseStatus.Code401({ errorMessage: responseStatus.WRONG_EMAIL_OR_PASSWORD }))
     }
     req.logIn(user, async function (err) {
       if (err) { return next(err) }
