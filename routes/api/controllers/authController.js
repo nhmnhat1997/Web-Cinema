8fdf66c292106dfb2d9ccf2be73836a860c46a03
Nguyen Hong Minh Nhat <1512293@hcmut.edu.vn>
2018-07-30 10:33:27 +0700

    Sua API Reset Password (khong gui duoc mail)

--- routes/api/controllers/authController.js
+++ routes/api/controllers/authController.js
@@ -5,6 +5,7 @@ var jwt = require('jsonwebtoken')
 var nodemailer = require('nodemailer')
 var passwordGenerator = require('password-generator')
 var ejs = require('ejs')
+var crypto = require('crypto-js')
 const User = mongoose.model('User')
 
 async function signUp (req, res, next) {
@@ -22,7 +23,7 @@ async function signUp (req, res, next) {
     }
     user = new User(userData)
     user.createdAt = Date.now()
-    user.password = user.hashPassword(userData.password)
+    user.password = User.hashPassword(userData.password)
     if (user.email && !isEmailValid(user.email)) {
       throw (responseStatus.Code409({ errorMessage: responseStatus.EMAIL_FORMAT }))
     }
@@ -55,28 +56,33 @@ async function sendResetPasswordMail (req, res, next) {
     if (!user) {
       throw (responseStatus.Code404({errorMessage: 'Không tìm thấy tài khoản. Vui lòng kiểm tra lại'}))
     }
+    console.log(user)
     let newPass = passwordGenerator(6)
-    user.password = User.hashPassword(newPass)
+    user.password = hashPassword(req.body.email, newPass)
     await user.save()
-    var smtpTransport = nodemailer.createTransport('SMTP', {
+    var smtpTransport = nodemailer.createTransport({
       service: 'Yahoo',
       auth: {
         user: 'nhmn040697@yahoo.com.vn',
-        pass: 'doremon123451986'
+        pass: 'jvngneihhwbeqqad'
       }
     })
     var content = ejs.render('<body style="font-family: Arial; font-size: 12px;"><div><p>Mật khẩu của bạn đã được đặt lại.</p><p>Mật khẩu mới của bạn là.</p><h2><%=newPass%></h2></div></body>', {newPass: newPass})
     var mailOptions = {
       to: req.body.email,
-      from: 'support@cinema.com',
+      from: 'nhmn040697@yahoo.com.vn',
       subject: 'Mật khẩu mới Cinema',
       html: content
     }
     smtpTransport.sendMail(mailOptions, function (err) {
-      if (err) { throw err }
+      if (err) {
+        console.log(err)
+        throw err
+      }
       res.send(responseStatus.Code200({message: 'Reset mật khẩu thành công.'}))
     })
   } catch (error) {
+    console.log(error)
     return error
   }
 }
@@ -84,6 +90,10 @@ async function sendResetPasswordMail (req, res, next) {
 function isEmailValid (email) {
   const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
   return re.test(String(email).toLowerCase())
+}
+
+function hashPassword (email, password) {
+  return crypto.AES.encrypt(password, email).toString()
 }
 
 module.exports = {
